31e6336d11abba868f7f9db2ccb09f94f4869ec4
Sergiy <sergiynesterenko90@gmail.com>
2018-02-18 11:52:26 -0800

    fetch using gdax python api

--- trader.py
+++ trader.py
@@ -1,4 +1,12 @@
 import gdax
 
+
 if __name__ == '__main__':
     print 'sup'
+
+    public_client = gdax.PublicClient()
+
+    ltcousd = public_client.get_product_historic_rates('LTC-USD', granularity='60')
+    ltcoeur = public_client.get_product_historic_rates('LTC-EUR', granularity='60')
+
+    print ltcousd
