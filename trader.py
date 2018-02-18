57d2a1ae0456f429b81e695b0126a2a11e1bae10
Sergiy <sergiynesterenko90@gmail.com>
2018-02-18 12:52:02 -0800

    added gitignore and price history plot

--- trader.py
+++ trader.py
@@ -1,6 +1,8 @@
 import requests
 import os
 import json
+import datetime
+import matplotlib.pyplot as plt
 
 class gdax_interface:
     def __init__(self):
@@ -23,12 +25,57 @@ class gdax_interface:
 
         return self.get(request_url)
 
+    def get_historic_rates(self, start, stop, granularity = '60'):
+        request_url = '/products/' + self.product_id + '/candles'
+        request_url += '?start=' + str(start)
+        request_url += '?stop=' + str(stop)
+        request_url += '?granularity=' + str(granularity)
+
+        return self.get(request_url)
+
+    def plot_historic_rates(self, json_response):
+        keys = ['time', 'low', 'high', 'open', 'close', 'volume']
+
+        arrs = zip(*json_response)
+
+
+        f, axarr = plt.subplots(len(keys)-1, sharex=True)
+
+
+
+
+        for idx, val in enumerate(keys):
+            if idx == 0:
+                continue
+
+            time = arrs[0]
+
+            axarr[idx-1].plot(time, arrs[idx])
+            axarr[idx-1].set_title(val)
+            axarr[idx-1].grid()
+
+
+        plt.show()
+
+
+
+
 
 
 if __name__ == '__main__':
     script_dir = os.path.dirname(os.path.realpath(__file__))
 
     gdax = gdax_interface()
-    json_out = gdax.get_order_book(level=2)
+    # json_out = gdax.get_order_book(level=2)
 
-    print json.dumps(json_out, indent=4)
+    now = datetime.datetime.now()
+    delta = datetime.timedelta(days=1)
+
+    start = (now - delta).isoformat()
+    stop = now.isoformat()
+
+    json_response = gdax.get_historic_rates(start,stop)
+
+    gdax.plot_historic_rates(json_response)
+
+    # print json.dumps(json_out, indent=4)
